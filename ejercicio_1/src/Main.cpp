7f1f00a27f82a1ad3a920a29de8f573f5886262e
anasofiahdzz <anasofiahdzz@ciencias.unam.mx>
2025-09-09 23:43:23 -0600

    main

--- ejercicio_1/src/Main.cpp
+++ ejercicio_1/src/Main.cpp
@@ -1,21 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include "Monstruo.h"
 
 // Declaramos la función enfrenta del namespace Batalla
 namespace Batalla {
-    void enfrenta(const Monstruo& m1, const Monstruo& m2, char atributo);
+    void enfrenta(const Monstruo& m1, const Monstruo& m2, char tipoBatalla);
 }
 
 int main() {
     std::cout << "MONSTER TOURNAMENT!!!\n";
 
-    Orco grommash("Grommash", 80, 60, 30);
-    Dragon alduin("Alduin", 50, 40, 90);
+    std::ifstream archivo("batallas.txt");
+    if (!archivo) {
+        std::cerr << "Error: no se pudo abrir batallas.txt\n";
+        return 1;
+    }
 
-    grommash.mostrarAtributos();
-    alduin.mostrarAtributos();
+    char especie1, especie2, tipo;
+    std::string nombre1, nombre2;
+    int f1, a1, i1;
+    int f2, a2, i2;
 
-    Batalla::enfrenta(grommash, alduin, 'f');
+    // Leer cada batalla desde el archivo
+    while (archivo >> especie1 >> nombre1 >> f1 >> a1 >> i1
+                   >> especie2 >> nombre2 >> f2 >> a2 >> i2 >> tipo) {
 
+        // Crear los monstruos según su especie
+        Monstruo* m1 = nullptr;
+        Monstruo* m2 = nullptr;
+
+        switch (especie1) {
+            case 'O': m1 = new Orco(nombre1, f1, a1, i1); break;
+            case 'D': m1 = new Dragon(nombre1, f1, a1, i1); break;
+            case 'T': m1 = new Troll(nombre1, f1, a1, i1); break;
+            case 'V': m1 = new Vampiro(nombre1, f1, a1, i1); break;
+            case 'E': m1 = new Esqueleto(nombre1, f1, a1, i1); break;
+            case 'H': m1 = new Hechicero(nombre1, f1, a1, i1); break;
+        }
+
+        switch (especie2) {
+            case 'O': m2 = new Orco(nombre2, f2, a2, i2); break;
+            case 'D': m2 = new Dragon(nombre2, f2, a2, i2); break;
+            case 'T': m2 = new Troll(nombre2, f2, a2, i2); break;
+            case 'V': m2 = new Vampiro(nombre2, f2, a2, i2); break;
+            case 'E': m2 = new Esqueleto(nombre2, f2, a2, i2); break;
+            case 'H': m2 = new Hechicero(nombre2, f2, a2, i2); break;
+        }
+
+        if (m1 && m2) {
+            Batalla::enfrenta(*m1, *m2, tipo);
+        }
+
+        delete m1;
+        delete m2;
+    }
+
+    archivo.close();
     return 0;
 }
